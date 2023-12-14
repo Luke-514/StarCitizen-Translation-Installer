32f354ca1d83fa3a20b54e4c3d28d83627608007
Luke514 <42091968+Luke-514@users.noreply.github.com>
2023-12-14 20:23:29 +0900

    日本語化解除を追加

--- Experimental Version/SCJPTI.bat
+++ Experimental Version/SCJPTI.bat
@@ -8,21 +8,7 @@ if "%1" == "" (
 )
 
 SET PLYVER=%1
-
-if not exist global.ini (
-  SET "counter=0"
-  curl -s https://api.github.com/repos/stdblue/StarCitizenJapaneseResources/releases --ssl-no-revoke | findstr "browser_download_url" | findstr "global.ini" > scjtdownload.lst
-
-  for /f "tokens=2" %%A in (scjtdownload.lst) do (
-      SET /a "counter+=1"
-      if !counter! equ 1 (
-          SET "url=%%~A"
-          curl -s -L !url! -O --ssl-no-revoke
-      )
-  )
-
-  del /Q scjtdownload.lst
-)
+SET LOGIC=%2
 
 for /f "tokens=*" %%i in ('findstr "libraryFolder" %APPDATA%\rsilauncher\logs\log.log') do SET LIBPATH=%%~i
 SET LIBPATH=%LIBPATH:libraryFolder": "=%
@@ -34,42 +20,87 @@ SET LOCALEDIR=%SCDIR%\data\Localization\japanese_"(japan)"
 SET GLOBALINIPATH=%LOCALEDIR%
 SET USERCFGPATH=%SCDIR%\user.cfg
 
-if not exist %GLOBALINIPATH% (
+if "%LOGIC%" == "Set" (
   if not exist global.ini (
-    exit 1
-  ) else (
-    mkdir %SCDIR%\data\Localization\japanese_"(japan)"
-    move /Y global.ini %GLOBALINIPATH% > nul
+    SET "counter=0"
+    curl -s https://api.github.com/repos/stdblue/StarCitizenJapaneseResources/releases --ssl-no-revoke | findstr "browser_download_url" | findstr "global.ini" > scjtdownload.lst
+
+    for /f "tokens=2" %%A in (scjtdownload.lst) do (
+        SET /a "counter+=1"
+        if !counter! equ 1 (
+            SET "url=%%~A"
+            curl -s -L !url! -O --ssl-no-revoke
+        )
+    )
+
+    del /Q scjtdownload.lst
   )
-) else (
-  if not exist global.ini (
-    exit 1
+
+  if not exist %GLOBALINIPATH% (
+    if not exist global.ini (
+      exit 1
+    ) else (
+      mkdir %SCDIR%\data\Localization\japanese_"(japan)"
+      move /Y global.ini %GLOBALINIPATH% > nul
+    )
   ) else (
-    move /Y global.ini %GLOBALINIPATH% > nul
-  )
-)
-
-if exist %USERCFGPATH% (
-
-  findstr g_language %USERCFGPATH% > nul
-
-  if !errorlevel! == 1 (
-    echo.>> %USERCFGPATH%
-    echo g_language = japanese_^(japan^)>> %USERCFGPATH%
-    echo g_languageAudio = english>> %USERCFGPATH%
-
-    if %errorlevel% == 1 (
-      exit 2
+    if not exist global.ini (
+      exit 1
+    ) else (
+      move /Y global.ini %GLOBALINIPATH% > nul
     )
   )
 
-) else (
-  echo g_language = japanese_^(japan^)> %USERCFGPATH%
-  echo g_languageAudio = english>> %USERCFGPATH%
+  if exist %USERCFGPATH% (
 
-  if not exist %USERCFGPATH% (
-    exit 3
+    findstr g_language %USERCFGPATH% > nul
+
+    if !errorlevel! == 1 (
+      echo.>> %USERCFGPATH%
+      echo g_language = japanese_^(japan^)>> %USERCFGPATH%
+      echo g_languageAudio = english>> %USERCFGPATH%
+
+      if %errorlevel% == 1 (
+        exit 2
+      )
+    )
+
+  ) else (
+    echo g_language = japanese_^(japan^)> %USERCFGPATH%
+    echo g_languageAudio = english>> %USERCFGPATH%
+
+    if not exist %USERCFGPATH% (
+      exit 3
+    )
   )
+  exit 0
 )
 
-exit 0
+if "%LOGIC%" == "Remove" (
+  if exist %USERCFGPATH% (
+
+    set JUDGE=FALSE
+
+    findstr "g_language" %USERCFGPATH% > nul
+    if !errorlevel! == 1 (
+      exit 5
+    ) else (
+      findstr /v "g_language" %USERCFGPATH%> user_cfg.tmp
+      EL=!errorlevel!
+      move user_cfg.tmp %USERCFGPATH% > nul
+
+      if !errorlevel! == 1 (
+        set JUDGE=TRUE
+      ) else if !EL! == 1 (
+        set JUDGE=TRUE
+      )
+
+      if !JUDGE! == TRUE (
+        exit 4
+      )
+    )
+  ) else (
+    exit 5
+  )
+  exit 0
+)
